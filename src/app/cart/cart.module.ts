9906a49711b8f6f99d268fb130ca7ab74b31a76f
anton_zinchenko <anton_zinchenko@mail.ru>
2019-10-07 21:01:24 +0300

    Linting error

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { GroupByPipe } from './pipes/group-by.pipe';
 import { shoppingCartRouterComponents, ShoppingCartRoutingModule } from './shopping-cart.routing.module';
 import { UserSelectionComponent, AddressFormComponent } from './pages/shopping-cart/components';
